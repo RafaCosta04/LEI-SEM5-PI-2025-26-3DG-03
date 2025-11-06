966ae61195c6cdc7cda6ff06ade7ff2c5c714d6c
joaosousa04 <1222123@isep.ipp.pt>
2025-11-06 16:49:13 +0000

    changes made to dock service issue #91

--- UI/src/app/services/docks.service.ts
+++ UI/src/app/services/docks.service.ts
@@ -11,30 +11,30 @@ export class DocksService {
   constructor(private apiService: ApiService) {}
 
   getAllDocks(): Observable<DocksModel[]> {
-    return this.apiService.get<DocksModel[]>('/Docks');
+    return this.apiService.get<DocksModel[]>('/Dock');
   }
 
   getDocksByName(name: string): Observable<DocksModel[]> {
-    return this.apiService.get<DocksModel[]>(`/Docks/ByName/${name}`);
+    return this.apiService.get<DocksModel[]>(`/Dock/ByName/${name}`);
   }
 
   getDocksByLocation(location: string): Observable<DocksModel[]> {
-    return this.apiService.get<DocksModel[]>(`/Docks/ByLocation/${location}`);
+    return this.apiService.get<DocksModel[]>(`/Dock/ByLocation/${location}`);
   }
 
   getDocksById(id: number): Observable<DocksModel> {
-    return this.apiService.get<DocksModel>(`/Docks/ByID/${id}`);
+    return this.apiService.get<DocksModel>(`/Dock/ByID/${id}`);
   }
 
   getDocksByVesselType(vesselType: string): Observable<DocksModel[]> {
-    return this.apiService.get<DocksModel[]>(`/Docks/ByVesselType`);
+    return this.apiService.get<DocksModel[]>(`/Dock/ByVesselType`);
   }
 
   createDock(dock: DocksModel): Observable<DocksModel> {
-    return this.apiService.post<DocksModel>('/Docks', dock);
+    return this.apiService.post<DocksModel>('/Dock', dock);
   }
 
   updateDock(id: number, dock: DocksModel): Observable<any> {
-    return this.apiService.put<any>(`/Docks/Update/${id}`, dock);
+    return this.apiService.put<any>(`/Dock/Update/${id}`, dock);
   }
 }
