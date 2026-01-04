82408a35aa3d13cc33f55d261bc3ef93e68b705d
rafa <1231031@isep.ipp.pt>
2026-01-04 18:05:45 +0000

    OEM-C mudule removed from the project

--- OEM-C/Domain/Factory/Incident/IIncidentTypeFactory.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Factory;
-
-
-using Domain.Model;
-
-public interface IIncidentTypeFactory
-{
-    IncidentType NewIncidentType(string code, string name, string description, IncidentClassification classification, IncidentType? parent = null);
-}
